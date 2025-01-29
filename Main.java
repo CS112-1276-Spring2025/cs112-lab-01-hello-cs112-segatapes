1f0375ebc3da5d99d11bcd653cb30237722f55a7
segatapes <rebeccalanari@outlook.com>
2025-01-29 23:48:18 +0000

    Finished part 2

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to CS 112!");
+        
+        System.out.println("Do I like video games? " + Main.isALike("video games"));
+        System.out.println("Do I like boxing? " + Main.isALike("boxing"));
+
+        Main.printAboutMe();
+    }
+
+    public static void printAboutMe() {
+        System.out.println("I like video games, gamedev, music, manga, anime, and writing.");
+    }
+
+    public static boolean isALike(String topic) {
+        String myLikes = "I like video games, game development, music, manga, anime, and writing.";
+        return myLikes.contains(topic);
     }
 }
