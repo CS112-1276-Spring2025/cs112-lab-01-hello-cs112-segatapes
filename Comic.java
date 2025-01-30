ca74580a167814983b8b1d122ba1a216f6ac6b26
segatapes <rebeccalanari@outlook.com>
2025-01-30 01:16:53 +0000

    Completed Lab

---
+++ Comic.java
@@ -0,0 +1,52 @@
+public class Comic {
+    // INSTANCE VARIABLES
+     
+    private String writer;
+    private String illustrator;
+    private String colorist;
+    private String title;
+    private int issueNum;
+
+    //MUTATORS and ACCESSORS
+
+    //Getter and Setter for Writer
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    //Getter and Setter for Illustrator
+    public void setIllustrator(String illustrator) {
+        this.illustrator = illustrator;
+    }
+
+    public String getIllustrator() {
+        return illustrator;
+    }
+    
+    //Getter and Setter for Colorist
+    public void setColorist(String colorist) {
+        this.colorist = colorist;
+    }
+
+    public String getColorist() {
+        return colorist;
+    }
+
+    //Getter and Setter for Title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle()
+
+    //Getter and Setter for Issue Num
+    public void setIssueNum(int issueNum) {
+        this.issueNum = issueNum;
+    }
+
+    
+}
