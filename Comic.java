3a57d1943e4a55d13a37191d06ba7019eaccba3d
segatapes <rebeccalanari@outlook.com>
2025-01-30 05:36:38 +0000

    In Class Lab Push

--- Comic.java
+++ Comic.java
@@ -1,13 +1,45 @@
 public class Comic {
     // INSTANCE VARIABLES
-     
+
     private String writer;
     private String illustrator;
     private String colorist;
     private String title;
     private int issueNum;
 
-    //MUTATORS and ACCESSORS
+
+    /*
+     * CONSTRUCTORS
+     */
+    
+    public Comic(String writer, String illustrator, String colorist, String title, int issueNum) {
+        this.setWriter(writer);
+        this.setIllustrator(illustrator);
+        this.setColorist(colorist);
+        this.setTitle(title);
+        this.setIssueNum(issueNum);
+    }
+
+    public Comic() {
+        this("Jane Doe", "John Doe", "Jorge Doe", "Comic", 0);
+    }
+
+    public Comic(Comic original) {
+        if (original == null) {
+            System.out.println("ERROR: trying to copy null object!");
+            System.exit(0);
+        } else {
+            this.setWriter(original.writer);
+            this.setIllustrator(original.illustrator);
+            this.setColorist(original.colorist);
+            this.setTitle(original.title);
+            this.setIssueNum(original.issueNum);
+        }
+    }
+
+    /*
+     * MUTATORS and ACCESSORS
+     */
 
     //Getter and Setter for Writer
     public void setWriter(String writer) {
@@ -41,12 +73,40 @@ public class Comic {
         this.title = title;
     }
 
-    public String getTitle()
+    public String getTitle() {
+        return title;
+    }
 
     //Getter and Setter for Issue Num
     public void setIssueNum(int issueNum) {
         this.issueNum = issueNum;
     }
 
+    public int getIssueNum() {
+        return issueNum;
+    }
+    
+    // Override toString method to provide a string representation of the Comic
+    // object
+
+    @Override
+    public String toString() {
+        return "Comic{" +
+                "illustrator='" + illustrator + '\'' +
+                ", writer='" + writer + '\'' +
+                ", colorist='" + colorist + '\'' +
+                ", title='" + title + '\'' +
+                ", issueNumber=" + issueNum +
+                '}';
+
+    }
+
+    public boolean equals(Comic otherComic) {
+
+        if (otherComic == null) {
+            return false;
+        }
+        return this.writer.equals(otherComic.writer) && this.illustrator.equals(otherComic.illustrator) && this.colorist.equals(otherComic.colorist) && this.title.equals(otherComic.title) && this.issueNum == otherComic.issueNum;
+    }
     
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to CS 112!");
-        
+        System.out.println("Hi! I'm a change! :)");
+
         System.out.println("Do I like video games? " + Main.isALike("video games"));
         System.out.println("Do I like boxing? " + Main.isALike("boxing"));
-
         Main.printAboutMe();
+
+        //String writer, String illustrator, String colorist, String title, int issueNum
+        Comic myFavorite = new Comic("Urasawa Naoki", "Urasawa Naoki", "Urasawa Naoki", "Monster", 1);
+        Comic nerysFavorite = new Comic("Stan Lee", "Ramos", "Delgado", "Spidey", 50);
+
+
+        nerysFavorite.setTitle("Amazing Spider-Man");
+        nerysFavorite.setIssueNum(1);
+
+        System.out.println("My favorite comic is " + myFavorite);
+        System.out.println("Nery's favorite comic is " + nerysFavorite);
+
+        System.out.println("Is Nery's Favorite Comic the same as mine? " + nerysFavorite.equals(myFavorite));
+
+
     }
 
     public static void printAboutMe() {
